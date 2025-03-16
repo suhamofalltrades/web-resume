ddc81e3a5175eeef9d654eb238617dbf7142b00a
suhamofalltrades <165378847+suhamofalltrades@users.noreply.github.com>
2025-03-16 14:51:38 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.getElementById("sideMenu");
+    if (menu.style.left === "0px") {
+        menu.style.left = "-250px";
+    } else {
+        menu.style.left = "0px";
+    }
+}
+
+function toggleDropdown(id) {
+    const dropdown = document.getElementById(id);
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
